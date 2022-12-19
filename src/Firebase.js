c0bbea4253709acff7ed840100e849c1122681f9
tomastes <51926325+tomastes@users.noreply.github.com>
2022-12-19 12:12:56 +0100

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,1 @@
-import firebase from 'firebase'
 
-
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAYpL-QKSHCCaBtDzcReT81Oid31yGXFRg",
-    authDomain: "clone-839b7.firebaseapp.com",
-    databaseURL: "https://clone-839b7.firebaseio.com",
-    projectId: "clone-839b7",
-    storageBucket: "clone-839b7.appspot.com",
-    messagingSenderId: "76185246215",
-    appId: "1:76185246215:web:05800e9ecdcb3d36fc28a2",
-    measurementId: "G-M3Z37JYPK4"
-  });
-
-const db = firebaseApp.firestore()
-const auth = firebase.auth()
-export {auth,db}
